b0565f22b15fdcde0d6f81b5b3430e353f72d00a
joyep-jojo <jo.yep.arce@gmail.com>
2021-06-21 09:42:23 -0500

    fixed typo
    fixed typo

--- utils.py
+++ utils.py
@@ -3,8 +3,8 @@ from datetime import datetime, timedelta
 import requests
 
 
-def calculate_earth_time_with_martian_sol(landing_time, martial_sol):
-    mars_date_seconds = int(martial_sol) * 88775
+def calculate_earth_time_with_martian_sol(landing_time, martian_sol):
+    mars_date_seconds = int(martian_sol) * 88775
     earth_time_seconds = mars_date_seconds / 86400
 
     time = int(earth_time_seconds)
